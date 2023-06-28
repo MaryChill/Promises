b2f7a1b76b1d2a485e1be402133b4a0fe47f332f
Mary Chill <chilikina.mari@inbox.ru>
2023-06-28 18:50:39 +0300

    first

---
+++ src/GameSavingLoader.js
@@ -0,0 +1,13 @@
+export default class GameSavingLoader {
+  static load() {
+    return new Promise((resolve) => {
+      const data = read();
+      data.then((response) => {
+        const value = json(response);
+        return value;
+      }).then((value) => {
+        resolve(value);
+      });
+    });
+  }
+}
